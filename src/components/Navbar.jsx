c2d11f3821f9aa4f9bff83aa89946be809e9e6c0
mayank chauhan <bobmayank1999@gmail.com>
2022-09-15 18:52:31 +0530

    home link added to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,12 @@ function Navbar(props) {
   return (
     <div className="bg-white py-5">
       <div className="relative flex flex-col items-center gap-4 w-4/5 mx-auto sm:flex-row sm:justify-between sm:w-4/5 sm:mx-auto">
-        <div className="flex flex-col items-center sm:items-start">
-          <h1 className="font-black text-gray-500 uppercase tracking-wide sm:tracking-wider sm:text-xl lg:text-2xl">The CodeYogi Shop</h1>
-          <p className="text-xs font-bold text-gray-400 tracking-wide sm:text-sm sm:tracking-wider lg:text-lg">Shop Your Coding Atire</p>
-        </div>
+        <Link to="/">
+          <div className="flex flex-col items-center sm:items-start">
+            <h1 className="font-black text-gray-500 uppercase tracking-wide sm:tracking-wider sm:text-xl lg:text-2xl">The CodeYogi Shop</h1>
+            <p className="text-xs font-bold text-gray-400 tracking-wide sm:text-sm sm:tracking-wider lg:text-lg">Shop Your Coding Atire</p>
+          </div>
+        </Link>
         <Link to="/cart">
         <HiOutlineShoppingBag className="text-5xl text-gray-400 lg:text-6xl">
         </HiOutlineShoppingBag>
